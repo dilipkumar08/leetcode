3fb611d8e455cebc1c4cec0c58345da52e2d7fcc
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-08-10 21:58:36 +0530

    Create add_binary.py

---
+++ add_binary.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        sum=bin(int(a,2)+int(b,2))[2:]
+        return sum
+      
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        c=int(a,2)
+        d=int(b,2)
+        return bin(c+d)[2:]
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a,2)+int(b,2))[2:]
