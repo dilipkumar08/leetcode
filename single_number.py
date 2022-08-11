2871cdab75303f1bab67994c2d5c630a6d6bfe1c
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-08-11 23:59:07 +0530

    Update single_number.py

--- single_number.py
+++ single_number.py
@@ -6,3 +6,24 @@ class Solution:
         return i
             
                 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        l=len(nums)
+        if l==1:
+            return nums[0]
+        else:
+            nums.sort()
+            for i in range(l):
+                if i==0:
+                    if nums[i]!=nums[i+1]:
+                        break
+                elif i==l-1:
+                    if nums[l-2]!=nums[l-1]:
+                        break
+                elif 0<i<l-1:
+                    if nums[i]!=nums[i+1] and nums[i]!=nums[i-1]:
+                        break
+        return nums[i]
+                    
+            
+                
