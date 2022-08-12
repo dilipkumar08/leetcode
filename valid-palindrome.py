b00664697e08b8d29e112256766393abfd942f8b
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-08-12 23:44:02 +0530

    Update valid-palindrome.py

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -6,3 +6,14 @@ class Solution:
             return "false"
         else:
             return "true"
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string=""
+        for i in s:
+            if i.isalnum():
+                string+=i
+        if string==string[::-1]:
+            return "true"
+        else:
+            return "false"
