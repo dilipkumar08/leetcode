56b841ebe2f9c418d2c2d51a07de4374f1be2798
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-08-22 22:26:43 +0530

    Update romantointeger.py

--- romantointeger.py
+++ romantointeger.py
@@ -1,34 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        l=0
+        i=0
         answer = 0
+        l=len(s)
+        nums =	{"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
         
-        nums =	{
-          "I": 1,
-          "V": 5,
-          "X": 10,
-          "L": 50,
-          "C": 100,
-          "D": 500,
-          "M": 1000,
-            
-        }
+        subs = {"IV": 4,"IX": 9,"XL": 40,"XC": 90,"CD": 400,"CM": 900}
         
-        subs = {
-            "IV": 4,
-            "IX": 9,
-            "XL": 40,
-            "XC": 90,
-            "CD": 400,
-            "CM": 900
-        }
-        
-        while l < len(s):
-            if s[l:l+2] in subs:
-                answer = answer + subs[s[l:l+2]]
-                l += 2
+        while i< l:
+            if s[i:i+2] in subs:
+                answer += subs[s[i:i+2]]
+                i+= 2
             else:
-                answer = answer + nums[s[l]]
-                l += 1
+                answer += nums[s[i]]
+                i += 1
                 
         return answer
