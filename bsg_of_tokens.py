50d34525254ebe55c725811c32630db56835dc7e
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-09-14 09:18:10 +0530

    Update bsg_of_tokens.py

--- bsg_of_tokens.py
+++ bsg_of_tokens.py
@@ -4,20 +4,26 @@ class Solution:
         l=len(tokens)
         n=0
         i=0
-        while True:
-            if l==0:
-                break
-            if l==1 and power<tokens[i]:
-                break
-            if tokens[i]<=power:
-                power=power-tokens[i]
-                n+=1
-                tokens.pop(i)
-                l-=1
-            if l<1:
-                break
-            if n>0 and tokens[i]>power and l>1:
-                power+=max(tokens) 
-                n-=1
+        if l==0:
+            n=0
+        elif min(tokens)>power:
+            n=0
+        else:
+            while True:
+                if tokens[i]<=power:
+                    power=power-tokens[i]
+                    n+=1
+                    tokens.pop(i)
+                    l-=1
+                    if l==1 and tokens[i]>power:
+                        break
+                    if l==0:
+                        break
+                if n>0 and tokens[i]>power and l>1:
+                    power+=tokens[l-1]
+                    tokens.remove(tokens[l-1])
+                    l-=1
+                    n-=1
+                
                 
         return n
