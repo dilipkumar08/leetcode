3fd4147a8665f4eac9ea2f55109a62ba310b34c8
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-11-09 22:26:40 +0530

    Update make_the_string_great.py
    Given a string s of lower and upper case English letters.
    
    A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
    
    0 <= i <= s.length - 2
    s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
    To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
    
    Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
    
    Notice that an empty string is also good.

--- make_the_string_great.py
+++ make_the_string_great.py
@@ -3,24 +3,19 @@ class Solution:
         li=list(x for x in s)
         l=len(li)
         i=0
-        if l==2:
-            if li[i]==li[i+1].upper() or li[i].upper==li[i+1] :
-                return ""
-            else:
-                return s
-        
-        while i+1<l:
-            if li[i+1]==li[i].upper() :
-                li.pop(i+1)
-                li.pop(i)
-                l-=2
-                i-=2
-            elif l==2 and li[i]==li[i-1].upper():
-                li.pop(i-1)
-                li.pop(i)
-                l-=2
-                i-=1
-            if l==0:
+        while i<l:
+            try:
+                if li[i]!=li[i+1]:
+                    if li[i+1]==li[i].upper() or li[i+1].upper() == li[i]:
+                        li.pop(i+1)
+                        li.pop(i)
+                        i=0
+                        l-=2
+                    else:
+                        i+=1
+                else:
+                    i+=1
+            except:
                 break
-            i+=1
+            
         return str("".join(li))
