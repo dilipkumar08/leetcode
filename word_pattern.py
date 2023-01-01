0566ff4099f5b8a39b6235bb01b228a4a8337daf
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2023-01-02 00:05:08 +0530

    Update word_pattern.py
    Given a pattern and a string s, find if s follows the same pattern.
    
    Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.

--- word_pattern.py
+++ word_pattern.py
@@ -3,20 +3,29 @@ class Solution:
         li=list(s.split(" "))
         l=len(li)
         p=len(pattern)
+        se=set()
         if p<1 and l<1:
             return True
         elif p!=l:
             return False
+        elif len(set(li))!=len(set(pattern)):
+            return False
         else:
             d={}
-            for i in range(len(li)):
+            for i in range(l):
+                print(i)
                 try:
                     d[pattern[i]]+=[li[i]]
                 except:
                     d[pattern[i]]=[li[i]]
+                print(d[pattern[i]])
                 if len(set(d[pattern[i]]))>1:
                     return False
-            if len(set(d.values()))!=len(d):
+                print(li[i],i,l)
+                se.add(li[i])
+                print(se)
+
+            if len(se)!=len(d):
                 return False
             else:
                 return True
