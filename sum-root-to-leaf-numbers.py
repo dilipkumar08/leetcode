0b17ac4425a631560ee11fc5880cee6f1fbaee7a
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2024-04-15 22:47:37 +0530

    Update sum-root-to-leaf-numbers.py

--- sum-root-to-leaf-numbers.py
+++ sum-root-to-leaf-numbers.py
@@ -6,12 +6,12 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def dft(cur,nums):
-            if not cur:
+        def rtl(node,num):
+            if not node:
                 return 0
-            nums=nums*10+cur.val
+            num=(num*10)+node.val
             
-            if not cur.right and not cur.left:
-                return nums
-            return dft(cur.right,nums)+dft(cur.left,nums)
-        return dft(cur=root,nums=0)
+            if not node.left and not node.right:
+                return num
+            return rtl(node.left,num)+rtl(node.right,num)
+        return rtl(root,0)
