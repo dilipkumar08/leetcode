27d4d02322f4958e6fe8222b4b88ca33e9cab112
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2024-06-15 00:13:14 +0530

    Update minimum-increment-to-make-array-unique.py

--- minimum-increment-to-make-array-unique.py
+++ minimum-increment-to-make-array-unique.py
@@ -3,14 +3,32 @@ class Solution:
         count=Counter(nums)
         res=0
         temp=0
-        for i in count:
+        for i in nums:
             temp=i
+            print(temp)
             while count[i]>1:
-                print(temp,count,res)
+
                 if temp in count:
                     res+=1
                     temp+=1
                 else:
+                    count[temp]=1
                     count[i]-=1 
+                    break
+        return res
+        
+----------------------------------------------------------------------
+#idle
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        res=0
+        temp=0
+        for i in range(len(nums)+max(nums)):
+            if count[i]>1:
+                extra=count[i]-1
+                count[i+1]+=extra
+                res+=extra
         
         return res
